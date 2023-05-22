fc68c82482972b1f6d3c5111e30806588e17fe5d
WhoopsImTop <eliasenglen2@gmail.com>
2023-05-22 16:02:19 +0200

    add new

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,66 +3,59 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Ansgar Hufnagel',
+    title: "Ansgar Hufnagel",
     htmlAttrs: {
-      lang: 'de'
+      lang: "de",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-      { meta: "Author", content: "khri8! more playgrounds."},
-      { meta: 'Programmierung', content: 'khri8! more playgrounds.'},
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
+      { meta: "Author", content: "khri8! more playgrounds." },
+      { meta: "Programmierung", content: "khri8! more playgrounds." },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'logo.png' },
-      { rel: 'stylesheet', href: 'https://use.typekit.net/btr6teo.css' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "logo.png" },
+      { rel: "stylesheet", href: "https://use.typekit.net/btr6teo.css" },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    './assets/main.css'
-  ],
+  css: ["./assets/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-    '@nuxtjs/markdownit',
-    '@nuxtjs/robots',
-    'nuxt-compress'
+    "@nuxt/content",
+    "@nuxtjs/markdownit",
+    "@nuxtjs/robots",
+    "nuxt-compress",
   ],
 
   markdownit: {
-    preset: 'default',
+    preset: "default",
     linkify: true,
     breaks: true,
-    use: [
-      'markdown-it-div',
-      'markdown-it-attrs'
-    ]
+    use: ["markdown-it-div", "markdown-it-attrs"],
   },
 
-  'nuxt-compress': {
+  "nuxt-compress": {
     gzip: {
       threshold: 8192,
     },
@@ -74,13 +67,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
